0e34bcb4b7b2e8d5317bc6322d998c300250caa6
MaximRock <maxrockvardill@gmail.com>
2026-05-12 21:32:56 +0300

    feat(nixos): переход на утилиту nixos-rebuild-ng
    изменил файл zsh.nix
    
    изменил aliases

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -6,15 +6,15 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-    initContent = ''fastfetch'';
+    initContent = "fastfetch";
     shellAliases = {
       nrs = "sudo nixos-rebuild-ng switch";
-      nrd = "sudo nixos-rebuild dry-build";
-      nrlg = "sudo nixos-rebuild list-generations";
+      nrd = "sudo nixos-rebuild-ng dry-build";
+      nrlg = "sudo nixos-rebuild-ng list-generations";
       nix-test = "nix flake check && sudo nixos-rebuild test";
       btw = "echo i use nixos, max";
       koda = "npx @kodadev/koda-cli@latest";
-      kill-qwen = ''pkill -9 -f qwen-code; sleep 2'';
+      kill-qwen = "pkill -9 -f qwen-code; sleep 2";
       #qc = ''nix run github:numtide/llm-agents.nix#qwen-code'';
     };
     oh-my-zsh = {
