4f479b067d0886f0b1ea3f363e0e946e7badcf27
MaximRock <maxrockvardill@gmail.com>
2026-05-30 20:25:30 +0300

    feat(editors): изменил модуль home/common/editors.nix
    добавлины настройки для indent-rainbow

--- home/common/editors.nix
+++ home/common/editors.nix
@@ -9,6 +9,16 @@
       oderwat.indent-rainbow
     ];
     extraSettings = {
+      # -- indent-rainbow --
+      # Цветные линии
+      "indentRainbow.indicatorStyle" = "light";
+      "indentRainbow.lightIndicatorStyleLineWidth" = 1;
+      "indentRainbow.colors" = [
+        "rgba(255,255,64,0.3)"
+        "rgba(127,255,127,0.3)"
+        "rgba(255,127,255,0.3)"
+        "rgba(79,236,236,0.3)"
+      ];
     };
   };
 
