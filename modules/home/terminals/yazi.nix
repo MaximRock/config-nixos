45066931999b00b56a84f08c0834a97a92bbd802
MaximRock <maxrockvardill@gmail.com>
2026-05-31 18:21:31 +0300

    feat(yazi): отредактировал файл yazi.nix
    добавил отображение размера файлов и директорий
    
    удалил закоментированные строки

--- modules/home/terminals/yazi.nix
+++ modules/home/terminals/yazi.nix
@@ -27,8 +27,9 @@
     [mgr]
     ratio          = [ 2, 4, 3 ]
     show_hidden = true
-    sort_by = "natural"
+    sort_by = "size"
     sort_dir_first = true
+    linemode = "size"
 
     [preview]
     max_width = 800
